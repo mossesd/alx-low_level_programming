04ba3995d2b89acb6b7b7be64de507d87b4138d2
mosisa dejene <mosisadejene5@gmail.com>
2023-03-17 10:58:11 -0700

    if else statement

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - funciton that lists numbers
+ *
+ * Return: Always succes
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
