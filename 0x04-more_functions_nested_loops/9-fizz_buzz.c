ce1986d50484177cf5dd17eb2003260954c0b85f
mosisa dejene <mosisadejene5@gmail.com>
2023-03-23 06:12:50 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints buff buzz
+ * Return: always succes
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuff");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
