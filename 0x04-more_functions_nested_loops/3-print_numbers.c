3d76510e9b9d55b36c93bd8a9ce6da6a1e8215fc
mosisa dejene <mosisadejene5@gmail.com>
2023-03-23 04:06:04 -0700

    correction

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers with a new line
- * Return: Always succes
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-
 void print_numbers(void)
 {
-	for (int i = 48; i <= 57; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints most numbers
- * Return: always succes
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
  */
-
 void print_most_numbers(void)
 {
-	for (int i = 48; i <= 57; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
-	return (0);
+
+	_putchar('\n');
 }
