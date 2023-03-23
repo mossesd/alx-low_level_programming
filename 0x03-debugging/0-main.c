490768d41fe5fdca228d6197601b5d73695d17bc
mosisa dejene <mosisadejene5@gmail.com>
2023-03-22 23:53:04 -0700

    correction

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - checks if number is negative or positive
- *
- * Return: always succes
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
