a47adb63373555bfb73c8dcbc872b3649a112000
mosisa dejene <mosisadejene5@gmail.com>
2023-03-22 23:46:12 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - checks if number is negative or positive
+ *
+ * Return: always succes
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
