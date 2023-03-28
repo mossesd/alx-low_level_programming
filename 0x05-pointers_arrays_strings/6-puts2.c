32eba9ce39c2ad6ddba043826cea87509f7a5c3c
mosisa dejene <mosisadejene5@gmail.com>
2023-03-28 06:48:42 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * puts2 - prints some of the string
- * @str: the string to be printed
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
  */
-
 void puts2(char *str)
 {
-	int i = 0;
+	int count = 0;
 
-	while (str[i] % 2 == 0)
+	while (count >= 0)
 	{
-		_putchar(str[i]);
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
 }
