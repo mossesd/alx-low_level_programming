07dfacda612af72ec551ce6c0b21e4e4d088e03c
mosisa dejene <mosisadejene5@gmail.com>
2023-03-29 04:03:30 -0700

    git ignore file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "runner.h"
+
+/**
+ * _strcat - concatinets a astring
+ * @srs: the source 
+ * @dest: the destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
