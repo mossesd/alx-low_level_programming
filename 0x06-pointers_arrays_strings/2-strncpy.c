ce0062cc054ed383280eb2e03ab5be8832510983
mosisa dejene <mosisadejene5@gmail.com>
2023-03-29 23:32:56 -0700

    more pointers and arrays

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
-
 /**
  * _strncpy - copies a string
- * @dest: the destination
- * @src: the source 
- * @n: the number of bytes to be coppied
- * Return: Always succes
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes from src.
+ * Return: the pointer to dest.
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i <=n && src[i] != '\0'; i++)
-	{
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-		i++;
-	}
-	for (; i < n; i++)
+	for ( ; i < n; i++)
 		dest[i] = '\0';
+
 	return (dest);
 }
