a0f5f3520827272e73465a38e0bc6754a1ca0582
mosisa dejene <mosisadejene5@gmail.com>
2023-04-03 00:31:27 -0700

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "runner.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: the destination to be coppied to
+ * @src: the source to be coppied from
+ * @n: the size to be coppied
+ * Return: always succes
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
