4fb4ac20ab65b20258c4e926a29c6bd14ef63747
mosisa dejene <mosisadejene5@gmail.com>
2023-04-03 00:22:05 -0700

    more pointers and arrys

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "runner.h"
+
+/**
+ * _memset - fills memory area with a constant bytes
+ * @s: is memory area pointed
+ * @b: the constant going to fill s
+ * @n: the amount of bytes to bel filled
+ * Return: always succes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
