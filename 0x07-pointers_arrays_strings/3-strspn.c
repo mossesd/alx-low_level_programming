3bbf39f392f3503ffdef7c35d6f2dcba29db637b
mosisa dejene <mosisadejene5@gmail.com>
2023-04-03 05:00:30 -0700

    more pointers and arrays

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "runner.h"
+
+/**
+ * _strspn - gets the length of a prefix string
+ * @s: the string to be checked
+ * @accept: the checker
+ * Return: always succes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, bool;
+	i = 0;
+	j = 0;
+	
+	while (s[i] != '\0')
+	{
+		bool = 1;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				bool = 0;
+				break;
+			}
+			j++;
+		}
+		if (bool == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
