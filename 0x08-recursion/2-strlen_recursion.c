a4b0851972efb408b048948104aeac73a73a796c
mosisa dejene <mosisadejene5@gmail.com>
2023-04-05 01:21:10 -0700

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,16 +3,21 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string to be counted
+ * Return: always success
  */
 
 int _strlen_recursion(char *s)
 {
 	int count = 0;
-	
-	if (*s != '\0')
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
 	{
 		count++;
 		_strlen_recursion(s + 1);
 	}
-	return(count);
+	return (count);
 }
