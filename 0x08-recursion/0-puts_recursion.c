44470d1a002ce759c76a6e7ea9887d610cb444f3
mosisa dejene <mosisadejene5@gmail.com>
2023-04-05 00:59:33 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
-	while (i != '\0')
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
