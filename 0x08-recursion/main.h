baf52c28990bf98f1822e1214e36c8e0378ec3e3
mosisa dejene <mosisadejene5@gmail.com>
2023-04-05 04:54:58 -0700

    more pointers and arrays

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int power_operation(int n, int c);
 int is_prime(unsigned int n, unsigned int c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
