f6dc0bc97faf2f48bc7c5b0142afce3360cbb63e
mosisa dejene <mosisadejene5@gmail.com>
2023-04-05 01:10:21 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints reversed string
+ * @s: the string to be reversed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
