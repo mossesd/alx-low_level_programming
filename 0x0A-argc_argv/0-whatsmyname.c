9f89f0e4ce4be2e754c29dba24a1b336ae25a964
mosisa dejene <mosisadejene5@gmail.com>
2023-04-07 01:00:06 -0700

    argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the value of argv
+ * @argc: the number of command line arguments
+ * @argv: the value of the command line arguments
+ * Return: always succes
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
