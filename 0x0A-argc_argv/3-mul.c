e8711ba92462139ccb1cfdc5b83675714bee1ac6
mosisa dejene <mosisadejene5@gmail.com>
2023-04-07 05:23:09 -0700

    correction

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the value of argv
+ * @argc: the number of command line arguments
+ * @argv: the value of the command line arguments
+ * Return: always succes
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
