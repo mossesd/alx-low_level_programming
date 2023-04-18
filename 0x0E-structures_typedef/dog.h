69eb125e393d09f3736d9f20a26c5daa133dce0b
mosisa dejene <mosisadejene5@gmail.com>
2023-04-18 00:04:16 -0700

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 /**
- * struct dog - structure that holds all the information of the dog
+ * struct dog - structure that holds all the information of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: struct called "dog" that stores
+ * its name, age and its owner
  */
 
 struct dog
