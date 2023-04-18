9e3d161d0f48f85c0b52724795f696b1053b49f3
mosisa dejene <mosisadejene5@gmail.com>
2023-04-18 00:28:28 -0700

    structures

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints the information of the dog
+ * @d: the struct to be printed
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		if (!(d->owner))
+			printf("owner: (nil)\n");
+		else
+			printf("owner: %s\n", d->owner);
+	}
+}
