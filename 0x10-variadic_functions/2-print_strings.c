3a409223c5b56f00fad1553a2a6132f64807125b
mosisa dejene <mosisadejene5@gmail.com>
2023-04-20 04:59:39 -0700

    variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,9 +21,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-
-		if (separator && i < n - 1)
-			printf("%s", separator);
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
 	}
 
 	printf("\n");
